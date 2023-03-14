c361e87a9531316f77bf72239c09974028a68882
lyes DOUKI <lyes.douki@gmail.com>
2023-03-14 14:04:11 +0100

    boutton replay + ajout des images

--- src/main/java/com/projet/morpion/controller/PlayController.java
+++ src/main/java/com/projet/morpion/controller/PlayController.java
@@ -1,18 +1,24 @@
 package com.projet.morpion.controller;
 
 import com.projet.morpion.modelMorpion.Morpion;
+import com.projet.morpion.utilities.SceneManager;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class PlayController {
 
     @FXML
     private GridPane matriceDuJeu;
     private boolean isJoeurOne = true;
-
+    @FXML
+    private Button replay;
+    @FXML
+    private Button quit;
     private boolean isAbleToSearchWinner = false;
     private Morpion morpion = new Morpion();
 
@@ -24,14 +30,22 @@ public class PlayController {
         System.out.println("id de la case jouée " + idJouer);
         if(isJoeurOne) {
             morpion.afterPlayerOneMove(Integer.parseInt(idJouer.toString()));
-            positionJoue.setText("1");
+            ImageView img = new ImageView("C:\\projets\\javaFX\\MorpionFx\\resources\\images\\TicTacToe\\cross2.png");
+            img.setFitWidth(30);
+            img.setFitHeight(30);
+            positionJoue.setGraphic(img);
+            //positionJoue.setText("1");
             isJoeurOne = false;
             positionJoue.setDisable(true);
         }
         else
         {
             morpion.afterPlayerTwoMove(Integer.parseInt(idJouer.toString()));
-            positionJoue.setText("2");
+            ImageView img = new ImageView("C:\\projets\\javaFX\\MorpionFx\\resources\\images\\TicTacToe\\circle2.png");
+            img.setFitWidth(30);
+            img.setFitHeight(30);
+            positionJoue.setGraphic(img);
+            //positionJoue.setText("2");
             isJoeurOne = true;
             positionJoue.setDisable(true);
         }
@@ -46,6 +60,14 @@ public class PlayController {
         if(morpion.isEndGame())
         {
             System.out.println("fini");
+            replay.setOnAction(actionEvent -> {
+                SceneManager.getInstance().changeScene("matriceDuJeu.fxml");
+            });
+            replay.setVisible(true);
+            quit.setOnAction(actionEvent -> {
+                SceneManager.getInstance().changeScene("menu.fxml");
+            });
+            quit.setVisible(true);
         }
 
 
